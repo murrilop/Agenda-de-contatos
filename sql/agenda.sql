e1ff96d647cc6a5adf541bacffbd5b5d6a0fb2b1
murrilop <murilogonzagasilva14@gmail.com>
2024-11-07 16:05:31 -0300

    alterando banco de dados

--- sql/agenda.sql
+++ sql/agenda.sql
@@ -1,10 +1,6 @@
-CREATE DATABASE agenda;
-USE agenda;
-
-CREATE TABLE contacts(
-id INT UNSIGNED AUTO_INCREMENT,
-NAME VARCHAR (150),
-phone VARCHAR(20),
-observations VARCHAR(255),
-PRIMARY KEY(id)
+create table contacts(
+    id int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (150),
+    phone VARCHAR (20),
+    observation varchar(255)
 );
