f7d652eda848f57683d7fad2ba867223c968ff5b
murrilop <murilogonzagasilva14@gmail.com>
2024-11-07 16:50:29 -0300

    atualizando o index com a tabela de contatos

--- config/process.php
+++ config/process.php
@@ -5,6 +5,8 @@
     include_once("conection.php");
     include_once("url.php");
 
+    $contacts = [];
+
     $query = "SELECT * from contacts";
     
     $stmt = $conn->prepare($query);
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php include_once("templates/header.php"); ?>
-    <h1>Testando Agenda</h1>
-    <i class="fas fa-eye"></i>
+    <div class="container">
+        <?php if(isset($printMsg) && $printMsg != ''): ?>
+            <p id="msg"> <?php echo $printMsg; ?> </p>
+        <?php endif; ?>
+        <h1 id="main-title">Minha agenda</h1>
+        <?php if(count($contacts) > 0): ?>
+        <p>tem contatos</p>
+        <?php else: ?>
+            <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?php echo $BASE_URL ?>create.php">Clique aqui para adicionar</a>.</p>
+        <?php endif; ?>
+    </div>
 <?php include_once("templates/footer.php"); ?>
