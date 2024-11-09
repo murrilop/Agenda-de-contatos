983ebb7716c8fbedd9e6dfe21b110b1a146e6056
murrilop <murilogonzagasilva14@gmail.com>
2024-11-09 08:56:39 -0300

    criação da pagina principal

--- index.php
+++ index.php
@@ -5,7 +5,30 @@
         <?php endif; ?>
         <h1 id="main-title">Minha agenda</h1>
         <?php if(count($contacts) > 0): ?>
-        <p>tem contatos</p>
+        <table class="table" id="contacts-table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($contacts as $contact): ?>
+                    <tr>
+                        <td scope="row"><?php echo $contact["id"]; ?></td>
+                        <td scope="row"><?php echo $contact["name"]; ?></td>
+                        <td scope="row"><?php echo $contact["phone"]; ?></td>
+                        <td class="actions">
+                            <a href="#"><i class="fas fa-eye check-icon"></i></a>
+                            <a href="#"><i class="far fa-edit edit-icon"></i></a>
+                            <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+                        </td>
+                    </tr>
+                <?php endforeach ?>    
+            </tbody>
+        </table>
         <?php else: ?>
             <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?php echo $BASE_URL ?>create.php">Clique aqui para adicionar</a>.</p>
         <?php endif; ?>
